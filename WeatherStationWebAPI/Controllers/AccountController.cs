7faa0a2015ed3c6b0d29f6db0ca29200a83239ce
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-05-03 13:40:43 +0200

    Added AccountController

---
+++ WeatherStationWebAPI/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherStationWebAPI.Data;
+
+namespace WeatherStationWebAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
