3269ded9b060264e932a8ac2224b0a0638e9af4f
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-03 15:27:05 +0200

    aa

--- WeatherStationWebAPI/Startup.cs
+++ WeatherStationWebAPI/Startup.cs
@@ -10,7 +10,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeatherStationWebAPI.Data;
+using WeatherStationWebAPI.Models;
 using WeatherStationWebAPI.WebSocket;
 
 namespace WeatherStationWebAPI
@@ -36,7 +38,7 @@ namespace WeatherStationWebAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -55,6 +57,10 @@ namespace WeatherStationWebAPI
             app.UseRouting();
 
             app.UseAuthentication();
+
+            SeedUsers(context);
+            SeedWeatherLog(context);
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -64,6 +70,47 @@ namespace WeatherStationWebAPI
                     pattern: "{controller=Account}/{action=Login}");
                 endpoints.MapHub<WeatherHub>("/weatherHub"); //For SignalR
             });
+        }
+
+        public void SeedUsers(ApplicationDbContext context)
+        {
+            Place place1 = new Place() {Latitude = 10.10, Longitude = 10.10, PlaceName = "Himalaya"};
+            Place place2 = new Place() { Latitude = 11.10, Longitude = 11.10, PlaceName = "K2" };
+            Place place3 = new Place() { Latitude = 12.10, Longitude = 12.10, PlaceName = "Randers" };
+
+            WeatherLog firstlog = new WeatherLog()
+            {
+                LogTime =  Convert.ToDateTime("10-10-2021"),
+                LogPlace = place1,
+                Temperature = 24,
+                Humidity = 80,
+                AirPressure = 50
+            };
+
+            context.WeatherLogs.AddAsync(firstlog);
+
+            WeatherLog secondlog = new WeatherLog()
+            {
+                LogTime = Convert.ToDateTime("10-10-2021"),
+                LogPlace = place2,
+                Temperature = 24,
+                Humidity = 80,
+                AirPressure = 50
+            };
+
+            WeatherLog thirdlog = new WeatherLog()
+            {
+                LogTime = Convert.ToDateTime("10-10-2021"),
+                LogPlace = place3,
+                Temperature = 24,
+                Humidity = 80,
+                AirPressure = 50
+            };
+        }
+
+        public void SeedWeatherLog(ApplicationDbContext context)
+        {
+
         }
     }
 }
