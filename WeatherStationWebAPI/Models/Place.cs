71d7e83b2d08d79b5fc0b348f903152e0be09290
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-03 14:00:38 +0200

    added

--- WeatherStationWebAPI/Models/Place.cs
+++ WeatherStationWebAPI/Models/Place.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace WeatherStationWebAPI.Models
 {
     public class Place
     {
+        [Key]
         public int PlaceId { get; set; }
 
         public string PlaceName { get; set; }
--- WeatherStationWebAPI/Models/WeatherLog.cs
+++ WeatherStationWebAPI/Models/WeatherLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,16 @@ namespace WeatherStationWebAPI.Models
 {
     public class WeatherLog
     {
+        [Key]
         public int LogId { get; set; }
         public  DateTime LogTime { get; set; }
 
         public Place LogPlace { get; set; }
 
         public float Temperature { get; set; }
+
+        public int AirMoisture { get; set; }
+
+        public decimal AirPressure { get; set; }
     }
 }
