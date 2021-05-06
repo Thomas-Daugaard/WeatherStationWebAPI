e4146427f1fe1deb3f43068ccab9f790563c60b8
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-05-06 12:29:55 +0200

    Added first succesful test

--- WeatherStationWebAPI.Test.Unit/UnitTest1.cs
+++ WeatherStationWebAPI.Test.Unit/UnitTest1.cs
@@ -1,4 +1,17 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using WeatherStationWebAPI.Controllers;
+using WeatherStationWebAPI.Data;
+using WeatherStationWebAPI.Models;
 
 namespace WeatherStationWebAPI.Test.Unit
 {
@@ -12,7 +25,18 @@ namespace WeatherStationWebAPI.Test.Unit
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var user = new UserDto()
+                { FirstName = "Kurt", LastName = "Poulsen", Email = "kp@somemail.com", Password = "Password1234" };
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44301/api/");
+                var postTask = client.PostAsJsonAsync<UserDto>("account/register", user);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                Assert.That(result.StatusCode, Is.EqualTo("Created"));
+            }
         }
     }
 }
--- WeatherStationWebAPI/Controllers/WeatherLogsController.cs
+++ WeatherStationWebAPI/Controllers/WeatherLogsController.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WeatherStationWebAPI.Data;
 using WeatherStationWebAPI.Models;
 
