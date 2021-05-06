6cc2a3d833f8a3d2b45b4e72553b533bd4f1bdcd
Camilla <camilla.holmstoel@hotmail.com>
2021-05-06 12:17:12 +0200

    signalR og view added

--- SignedUpClient/wwwroot/js/site.js
+++ SignedUpClient/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("P:\NGK\Afl3\WeatherStationWebAPI\WebSocket\WeatherHub.cs").build();
+
+connection.on("UpdateMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("WeatherLog").appendChild(li);
+})
