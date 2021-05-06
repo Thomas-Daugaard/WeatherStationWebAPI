11cdc7e9ac78194dd0d07c405809428bac0ac506
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-06 11:30:26 +0200

    ddd

--- WeatherStationWebAPI/Models/User.cs
+++ WeatherStationWebAPI/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +19,8 @@ namespace WeatherStationWebAPI.Models
         public string Email { get; set; }
         [MaxLength(60)]
         public string PwHash { get; set; }
+
+        [ForeignKey("PlaceId")]
+        public List<Place> SignedUpPlaces { get; set; }
     }
 }
