a15519c364e4701c32a926f4365cd60db12597c2
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-08 22:09:16 +0200

    added tests for weathercontroller

---
+++ WeatherStationWebAPI.Test.Unit/WeatherLogControllerTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NUnit.Framework;
+using WeatherStationWebAPI.Controllers;
+using WeatherStationWebAPI.Data;
+using WeatherStationWebAPI.Models;
+using WeatherStationWebAPI.WebSocket;
+
+namespace WeatherStationWebAPI.Test.Unit
+{
+    public class WeatherLogControllerTest
+    {
+        private ApplicationDbContext _context;
+        private IOptions<AppSettings> _appSettings;
+        private IHubContext<WeatherHub> _mockHub;
+        private WeatherLogsController _weatherController;
+        private Place _place;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new ApplicationDbContext(
+                new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlServer(CreateInMemoryDatabase()).Options); 
+
+            //In memory DB for testing purpose
+                    static DbConnection CreateInMemoryDatabase()
+                    {
+                        var connection = new SqlConnection("Data Source=localhost;Initial Catalog=NGKWebApiWeatherLog;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                        connection.Open();
+                        return connection;
+                    }
+
+            var settings = new AppSettings()
+            {
+                SecretKey = "ncSK45=)7@#qwKDSopevvkj3274687236"
+            };
+
+            _place = Substitute.For<Place>();
+
+            _appSettings = Options.Create(settings);
+
+            _mockHub = Substitute.For<IHubContext<WeatherHub>>();
+
+            _weatherController = new WeatherLogsController(_context, _mockHub);
+        }
+
+        [Test]
+        public async Task GetWeatherLogs_isEQToSeeded9()
+        {
+            ActionResult<IEnumerable<WeatherLog>> logs = await _weatherController.GetWeatherLogs();
+
+            Assert.AreEqual(9, logs.Value.Count());
+
+        }
+
+        [TestCase(1)]
+        public async Task GetWeatherLogById_SeededLogs(int id)
+        {
+            ActionResult<WeatherLog> log = await _weatherController.GetWeatherLog(id);
+
+            Assert.AreEqual(24, log.Value.Temperature);
+        }
+
+    }
+}
